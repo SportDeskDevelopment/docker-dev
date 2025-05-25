740e1400d13a1e97700ab39d3b9c023736fbd059
IhorKravtsov <kravtsov.igor.dev@gmail.com>
2025-05-25 19:53:39 +0200

    Add support for optional Prisma Studio in start script

--- start.js
+++ start.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const concurrently = require("concurrently");
 
+const args = process.argv.slice(2);
+const isStudio = args.includes("--studio") || args.includes("-s");
+
 concurrently(
   [
     {
@@ -18,6 +21,11 @@ concurrently(
       command: "npm run dev",
       cwd: path.resolve(__dirname, "../web/"),
     },
+    ...(isStudio ? [{
+      name: "studio",
+      command: "npx prisma studio",
+      cwd: path.resolve(__dirname, "../backend/")
+    }] : []),
   ],
   {
     killOthers: true,
